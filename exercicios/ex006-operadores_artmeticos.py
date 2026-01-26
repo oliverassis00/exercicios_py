0f0622eebb187cc3ceb39d5fc40829909534e2e5
Oliver Assis <oliverinvest08@gmail.com>
2026-01-25 23:18:15 -0300

    exercicios de operadores aritmeticos

---
+++ exercicios/ex006-operadores_artmeticos.py
@@ -0,0 +1,12 @@
+# #Ler um número e mostrar o antecessor e o sucessor
+
+# digite_numero = int(input('Digite um número: '))
+
+# print(f'O seu antecessor é o número {digite_numero - 1} e o sucessor é o número {digite_numero + 1}!')
+
+
+#Leia um número e mostre o dobro, triplo e a raiz quadrada
+
+digite_algum_numero = int(input('Digite um número: '))
+
+print(f'O dobro desse número é {digite_algum_numero * 2} o triplo dele é {digite_algum_numero * 3} e a raiz quadrada dele é {digite_algum_numero ** (1/2)}')
